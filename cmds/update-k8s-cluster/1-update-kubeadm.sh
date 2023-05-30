c85ca1b3ff759a124c678ce72cfc164300594ae9
yokaimeow <yokaimeow@qq.com>
2023-05-30 09:21:13 +0800

    k8s version 1.27.2

--- cmds/update-k8s-cluster/1-update-kubeadm.sh
+++ cmds/update-k8s-cluster/1-update-kubeadm.sh
@@ -2,6 +2,6 @@
 
 yum list --showduplicates kubeadm --disableexcludes=kubernetes
 
-ansible all -m shell -a 'yum install -y kubeadm-{{VERSION}}-0 --disableexcludes=kubernetes' -e 'VERSION="1.26.4"'
+ansible all -m shell -a 'yum install -y kubeadm-{{VERSION}}-0 --disableexcludes=kubernetes' -e 'VERSION="1.27.2"'
 
 ansible all -m shell -a 'kubeadm version'
