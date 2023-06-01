262221ddf7dcf85f880c760ea8876fcd69bb09a1
yokaimeow <yokaimeow@qq.com>
2023-06-01 10:36:09 +0800

    add nfs storage

---
+++ cmds/nfs-subdir-external-provisioner/install-nfs-subdir-external-provisioner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
+
+helm repo update
+
+helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner   --namespace=nfs-provisioner   --create-namespace   --set replicaCount=1   --set storageClass.name=nfs-client   --set storageClass.defaultClass=true   --set nfs.server=192.168.50.171   --set nfs.path=/volume1/nfs
