db003bb610563fcd21ef05beb4aa375d69305c6e
yokaimeow <yokaimeow@qq.com>
2023-11-10 11:38:59 +0800

    Add: init vault

--- cmds/vault/install-vault.sh
+++ cmds/vault/install-vault.sh
@@ -5,3 +5,5 @@ helm repo add hashicorp https://helm.releases.hashicorp.com
 helm repo update
 
 helm install vault hashicorp/vault -n vault --create-namespace -f values.yaml
+
+kubectl exec -n vault --stdin=true --tty=true vault-0 -- vault operator init
