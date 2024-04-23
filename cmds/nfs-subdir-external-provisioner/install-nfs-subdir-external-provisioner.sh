552298744c246d137593bed31fa4fcb5796bc758
yokaimeow <yokaimeow@qq.com>
2024-04-23 15:03:56 +0800

    StorageClass Set reclaimPolicy to Retain

--- cmds/nfs-subdir-external-provisioner/install-nfs-subdir-external-provisioner.sh
+++ cmds/nfs-subdir-external-provisioner/install-nfs-subdir-external-provisioner.sh
@@ -4,4 +4,4 @@ helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/
 
 helm repo update
 
-helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner   --namespace=nfs-provisioner   --create-namespace   --set replicaCount=1   --set storageClass.name=nfs-client   --set storageClass.defaultClass=true   --set nfs.server=192.168.50.171   --set nfs.path=/volume1/nfs
+helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner --namespace=nfs-provisioner --create-namespace --set replicaCount=1 --set storageClass.name=nfs-client --set storageClass.defaultClass=true --set nfs.server=192.168.50.171 --set nfs.path=/volume1/nfs --set storageClass.reclaimPolicy=Retain
