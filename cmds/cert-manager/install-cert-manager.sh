dbbb78b163b074f5dcd9ad12998473082d80b578
yokaimeow <yokaimeow@qq.com>
2024-04-28 14:22:45 +0800

    update cert-manager

--- cmds/cert-manager/install-cert-manager.sh
+++ cmds/cert-manager/install-cert-manager.sh
@@ -4,6 +4,6 @@ helm repo add jetstack https://charts.jetstack.io
 
 helm repo update
 
-helm install cert-manager --namespace cert-manager --version v1.13.3 jetstack/cert-manager --set installCRDs=true
+helm install cert-manager --namespace cert-manager --version v1.14.5 jetstack/cert-manager --set installCRDs=true
 
-helm upgrade cert-manager --namespace cert-manager --version v1.13.3 jetstack/cert-manager --set installCRDs=true
+helm upgrade cert-manager --namespace cert-manager --version v1.14.5 jetstack/cert-manager --set installCRDs=true
