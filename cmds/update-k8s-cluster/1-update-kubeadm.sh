23f721fe823662ffafffc4c406f59bd9b0e9d099
yokaimeow <yokaimeow@qq.com>
2025-05-23 18:00:50 +0800

    Update cluster version to 1.33.1

--- cmds/update-k8s-cluster/1-update-kubeadm.sh
+++ cmds/update-k8s-cluster/1-update-kubeadm.sh
@@ -2,6 +2,6 @@
 
 yum list --showduplicates kubeadm --disableexcludes=kubernetes
 
-ansible all -m shell -a 'yum install -y kubeadm-{{VERSION}} --disableexcludes=kubernetes' -e 'VERSION="1.33.0"'
+ansible all -m shell -a 'yum install -y kubeadm-{{VERSION}} --disableexcludes=kubernetes' -e 'VERSION="1.33.1"'
 
 ansible all -m shell -a 'kubeadm version'
